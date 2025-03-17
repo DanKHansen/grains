0e8eb4cbf3f5ef3c745c079f8f7c528e28b6bc4b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-17 12:17:53 +0100

    solution

--- src/main/scala/Grains.scala
+++ src/main/scala/Grains.scala
@@ -1,4 +1,14 @@
 object Grains:
-   def square(n: Int): Option[BigInt] = ???
-   
-   val total: BigInt = ???
+   def square(n: Int): Option[BigInt] =
+      n match
+         case x if x < 1 || x > 64 => None
+         case y if y == 64         => Some(f(y) + 1)
+         case _                    => Some(f(n))
+
+   private def f(n: Int): BigInt = BigInt(math.pow(2, n - 1).toLong)
+
+   val total: BigInt =
+      val seq = for i <- 1 to 64 yield square(i) match
+         case Some(value) => value
+         case None        => BigInt(0)
+      seq.sum
