1c7b69f9230ce94264653e6bf04f38aafddad8dd
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-17 12:39:17 +0100

    Concise

--- src/main/scala/Grains.scala
+++ src/main/scala/Grains.scala
@@ -1,6 +1,5 @@
-object Grains {
+object Grains:
    def square(n: Int): Option[BigInt] =
-      if (n >= 1 && n <= 64) Some(BigInt(1) << (n - 1)) else None
+      if n >= 1 && n <= 64 then Some(BigInt(1) << (n - 1)) else None
 
    val total: BigInt = (1 to 64).flatMap(square).sum
-}
