d48f2bead8ea48da956f9f95e1bcc13e956aae2a
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-17 12:37:21 +0100

    Concise

--- src/main/scala/Grains.scala
+++ src/main/scala/Grains.scala
@@ -1,14 +1,6 @@
-object Grains:
+object Grains {
    def square(n: Int): Option[BigInt] =
-      n match
-         case x if x >= 1 && x <= 63 => Some(f(x))
-         case y if y == 64           => Some(f(y) + 1)
-         case _                      => None
+      if (n >= 1 && n <= 64) Some(BigInt(1) << (n - 1)) else None
 
-   private def f(n: Int): BigInt = BigInt(math.pow(2, n - 1).toLong)
-
-   val total: BigInt =
-      val seq = for i <- 1 to 64 yield square(i) match
-         case Some(value) => value
-         case None        => BigInt(0)
-      seq.sum
+   val total: BigInt = (1 to 64).flatMap(square).sum
+}
